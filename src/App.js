46f99bf3ace0b2e25909c9a1bfe4869ba90b0b04
phoebus2 <jihk0227@gmail.com>
2020-03-06 17:44:51 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React from 'react';
 
-function Food({fav}) {
-  console.log(fav)
-  return <h3>I Love {fav}!!!!</h3>
+const food = [
+  {
+    id : 1,
+    name : "newyork cheeze",
+    image : "https://cdn.dominos.co.kr/admin/upload/goods/20200305_djh7347I.jpg"
+  },
+  {
+    id : 2,
+    name : "chikesand",
+    image : "https://cdn.dominos.co.kr/admin/upload/goods/20200119_Dzj9GV1R.jpg"
+  },
+  {
+    id : 3,
+    name : "meetmeetmeet",
+    image : "https://cdn.dominos.co.kr/admin/upload/goods/20200119_h150V2hn.jpg"
+  },
+  {
+    id : 4,
+    name : "oct",
+    image : "https://cdn.dominos.co.kr/admin/upload/goods/20200119_6NVSk5po.jpg"
+  }
+]
+
+// function renderFood(dish){
+//   return <Food key={dish.id} name={dish.name} picture={dish.image} />
+// }
+
+function Food({name, picture}) {
+  return <div>
+      <h1>I Love {name}!!!</h1>
+      <img src={picture} alt={name} />
+  </div>
 }
 
 function App() {
   return (
   <div>
-    <h1>Hello!!!!asdfadf</h1>
-    <Food fav="kimchi" />
-    <Food fav="samgupsal" />
-    <Food fav="chuggumi" />
-    <Food fav="ramen" />
+      {food.map(dish => 
+        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+      )}}
+      {/* {console.log(food.map(renderFood))}
+      {food.map(renderFood)} */}
   </div>
   );
 }
