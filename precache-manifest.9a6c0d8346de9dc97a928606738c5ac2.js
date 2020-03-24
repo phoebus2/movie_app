531f70b545ce8dc9b0a8cd94fd4211ac79561680
Minsu Kim <jihk0227@gmail.com>
2020-03-25 06:52:08 +0900

    Updates

---
+++ precache-manifest.9a6c0d8346de9dc97a928606738c5ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5237eec2ebd0054134a127b57137353b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e56c693fa12c11ef14c4",
+    "url": "/movie_app/static/css/main.15f30942.chunk.css"
+  },
+  {
+    "revision": "5b34aa61a648f2d64abd",
+    "url": "/movie_app/static/js/2.8deb2921.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.8deb2921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56c693fa12c11ef14c4",
+    "url": "/movie_app/static/js/main.0bed23f0.chunk.js"
+  },
+  {
+    "revision": "c145c45984fe2e41280f",
+    "url": "/movie_app/static/js/runtime-main.ffbc15b4.js"
+  }
+]);
